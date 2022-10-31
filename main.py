3bbce00f8f870de02f7c7d4413aea77d58d80a69
Mahmoud <md.thunder2014@gmail.com>
2022-10-31 20:49:36 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+# tax = input("Enter Tax: ")
+# tax_one= float(tax)/100
+# price = input("Enter Price: ")
+# sum = float(price) * tax_one
+# a =sum +float(price)
+# print("Financial tax is: ",a)
+# # =========================================
+# def say_hi(user="mahmoud"):
+#     print("hello "+user)
+#
+# print("hello 1")
+# say_hi()
+# print("hello 2")
+
+#
+# def cube(num):
+#     return num*num*num
+#
+# result = cube(4)
+#
+# print(result)
+
+# =============================
+
+# a = 12
+# b = 32
+# if a > b:
+#     print("hello")
+# else:
+#     print("hi")
+
+
+# is_hungry = False
+# wants_to_eat = True
+#
+# if is_hungry and wants_to_eat:
+#     print("Go eat you are hungry And you want to eat")
+# elif is_hungry and not wants_to_eat:
+#     print("Eat So you can survive")
+# elif not is_hungry and  wants_to_eat:
+#     print("do not eat you are not hungry")
+# else:
+#     print("do not eat")
+
+# ===============================
+
+# def max_num(num1, num2, num3):
+#     if num1 >= num2 and num1 >= num3:
+#         return num1
+#     elif num2 >= num1 and num2 >= num3:
+#         return num2
+#     else:
+#         return num3
+#
+# max_q = max_num(-4343, 12, 232)
+# # OR
+# print(max(121, 54, 6, 3, 8))
+
+# ===============================
+
+# months = {
+#     "jan": "january",
+#     "feb": "february",
+#     "mar": "march"
+# }
+#
+
+# ==============================
